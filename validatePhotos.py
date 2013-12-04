e4e17d56bc78555ec394531df5c7cd17d0c1786b
Michael Peppler <mpeppler@carnegiehall.org>
2013-12-04 11:03:58 -0500

    validate photos can now be used to update photos that were added by id only. Fix only calling on mrniceguy

--- validatePhotos.py
+++ validatePhotos.py
@@ -10,27 +10,33 @@ saveCursor = conn.cursor()
 client = foursquare.Foursquare(client_id=CLIENT_ID, client_secret=CLIENT_SECRET)
 
 with conn.cursor(cursor_factory=psycopg2.extras.DictCursor) as cur:
-    cur.execute("SELECT * FROM trucks WHERE id='mrnice';")
+    cur.execute("SELECT * FROM trucks;")
 
     for truck in cur:
         print truck['id']
-        data = client.venues.photos(truck['foursquare'], params={})
-        photoItems = data['photos']['items']
-
         with conn.cursor(cursor_factory=psycopg2.extras.DictCursor) as imageCur:
             imageCur.execute("SELECT * FROM images WHERE twitname='" + truck['twitname']+ "';")
-            for image in imageCur:
-                found = False
-                for photo in photoItems:
-                    if image['id'] == photo['id']:
-                        found = True
-                        break
-                print image['id'] + " " + str(found)
-                if not found:
-                    # Need to see if maybe it's in the next 200
-                     saveCursor.execute(
-                        """UPDATE images SET visibility = (%(visibility)s) WHERE id = (%(id)s);""",
-                        {'visibility': 'private', 'id': image['id']})
+            if imageCur.rowcount > 0:
+                data = client.venues.photos(truck['foursquare'], params={})
+                photoItems = data['photos']['items']
+                for image in imageCur:
+                    found = False
+                    for photo in photoItems:
+                        if image['id'] == photo['id']:
+                            found = True
+                            break
+                    print image['id'] + " " + str(found)
+                    if found:
+                        saveCursor.execute(
+                            """UPDATE images SET prefix = (%(prefix)s), 
+                            suffix = (%(suffix)s), visibility = (%(visibility)s) WHERE id = (%(id)s);""",
+                            {'visibility': photo['visibility'], 'suffix' : photo['suffix'],
+                            'prefix' : photo['prefix'], 'id': image['id']})
+                    else:
+                        # Need to see if maybe it's in the next 200
+                         saveCursor.execute(
+                            """UPDATE images SET visibility = (%(visibility)s) WHERE id = (%(id)s);""",
+                            {'visibility': 'private', 'id': image['id']})
 
 conn.commit()
 saveCursor.close()
