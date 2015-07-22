f8e3ea90e69423dc256bb69c294dd647bed530d3
Michael Peppler <peppage@gmail.com>
2015-07-22 01:31:32 -0400

    refactor: move admin routes into own file

--- wanderinglunch.go
+++ wanderinglunch.go
@@ -119,24 +119,6 @@ func support(c web.C, w http.ResponseWriter, r *http.Request) {
 	renderer.HTML(w, http.StatusOK, "support", nil)
 }
 
-func adminRoot(c web.C, w http.ResponseWriter, r *http.Request) {
-	t1 := time.Now().Add(-24 * (time.Minute * 60)).Unix()
-	now := time.Now()
-	t2 := time.Date(now.Year(), now.Month(), now.Day(), 0, 0, 0, 0, now.Location()).Unix()
-	
-	trucks := []*Truck{}
-	err := db.Select(&trucks, `SELECT twitname, id FROM trucks WHERE lastupdate < $1 AND lastupdate > $2`, t1, t2)
-	if err != nil {
-		fmt.Println(err)
-	}
-
-	data := make(map[string]interface{})
-	data["trucks"] = trucks
-	data["title"] = "Wandering Lunch: NYC Food Truck Finder | Admin"
-
-	renderer.HTML(w, http.StatusOK, "admin.index", data)
-}
-
 func init() {
 	var err error
 	db, err = sqlx.Open("postgres", "user=mca dbname=foodtruck sslmode=disable")
