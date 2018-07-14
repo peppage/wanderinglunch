b401d2bf1a0811e78c51bae06a356f481c6806e0
Michael Peppler <peppage@gmail.com>
2018-07-14 11:31:42 -0400

    show different template if no updates

--- pages.go
+++ pages.go
@@ -1,6 +1,8 @@
 package main
 
 import (
+	"io"
+	"io/ioutil"
 	"net/http"
 
 	"github.com/CloudyKit/jet"
@@ -15,12 +17,18 @@ func index(w http.ResponseWriter, r *http.Request) {
 	}
 
 	template, _ := View.GetTemplate("index.jet")
+	if len(spots) == 0 {
+		template, _ = View.GetTemplate("index-notrucks.jet")
+	}
 
 	vars := make(jet.VarMap)
 	vars.Set("spots", spots)
 	if err := template.Execute(w, vars, nil); err != nil {
 		panic(err)
 	}
+
+	io.Copy(ioutil.Discard, r.Body)
+	defer r.Body.Close()
 }
 
 func allTrucks(w http.ResponseWriter, r *http.Request) {
@@ -37,6 +45,9 @@ func allTrucks(w http.ResponseWriter, r *http.Request) {
 	if err := template.Execute(w, vars, nil); err != nil {
 		panic(err)
 	}
+
+	io.Copy(ioutil.Discard, r.Body)
+	defer r.Body.Close()
 }
 
 func mapPage(w http.ResponseWriter, r *http.Request) {
@@ -45,7 +56,7 @@ func mapPage(w http.ResponseWriter, r *http.Request) {
 	if err != nil {
 		w.Write([]byte(err.Error()))
 	}
-	
+
 	template, _ := View.GetTemplate("alltrucks.jet")
 
 	vars := make(jet.VarMap)
@@ -53,4 +64,7 @@ func mapPage(w http.ResponseWriter, r *http.Request) {
 	if err := template.Execute(w, vars, nil); err != nil {
 		panic(err)
 	}
+
+	io.Copy(ioutil.Discard, r.Body)
+	defer r.Body.Close()
 }
