f12c7bac08c18ca769a616a791033fc9f5aa7a08
Michael Peppler <mpeppler@carnegiehall.org>
2018-09-19 16:09:27 -0400

    add in admin templates

--- pages.admin.go
+++ pages.admin.go
@@ -7,6 +7,15 @@ import (
 )
 
 func adminIndex(w http.ResponseWriter, r *http.Request) {
+	c := pageContext{
+		Version: Version,
+	}
+
+	template, _ := View.GetTemplate("admin/index.jet")
+	if err := template.Execute(w, nil, c); err != nil {
+		panic(err)
+	}
+
 	io.Copy(ioutil.Discard, r.Body)
 	defer r.Body.Close()
 }
