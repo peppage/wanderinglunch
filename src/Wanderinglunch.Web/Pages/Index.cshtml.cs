40ccd35cf8aab5b6877e3cb7b31edb4dca2fcf93
Michael Peppler <mpeppler@carnegiehall.org>
2019-07-08 17:08:03 -0400

    add relative time for homepage

--- src/Wanderinglunch.Web/Pages/Index.cshtml.cs
+++ src/Wanderinglunch.Web/Pages/Index.cshtml.cs
@@ -1,4 +1,4 @@
-﻿using System.Collections;
+﻿using System;
 using System.Collections.Generic;
 using System.Linq;
 using Microsoft.AspNetCore.Mvc;
@@ -15,6 +15,11 @@ namespace Wanderinglunch.Web.Pages
         public IEnumerable<string> Zones { get; set; }
 
         private readonly ILunchContext lunchContext;
+
+        const int SECOND = 1;
+        const int MINUTE = 60 * SECOND;
+        const int HOUR = 60 * MINUTE;
+        DateTime epoch = new DateTime(1970, 1, 1, 0, 0, 0, DateTimeKind.Utc);
 
         public IndexModel(ILunchContext lunchContext)
         {
@@ -38,6 +43,40 @@ namespace Wanderinglunch.Web.Pages
         {
             var trucks = Trucks.Where(t => t.Location.Zone == zone).Distinct(new SpotComparer());
             return trucks.OrderByDescending(t => t.Location.Lat);
+        }
+
+        public string RelativeTime(long truckEpoch)
+        {
+            var truckTime = epoch.AddSeconds(truckEpoch);
+            var ts = new TimeSpan(DateTime.UtcNow.Ticks - truckTime.Ticks);
+            var delta = Math.Abs(ts.TotalSeconds);
+
+            if (delta < 1 * MINUTE)
+            {
+                return ts.Seconds == 1 ? "one second ago" : $"{ts.Seconds} second ago";
+            }
+
+            if (delta < 2 * MINUTE)
+            {
+                return "a minute ago";
+            }
+
+            if (delta < 45 * MINUTE)
+            {
+                return $"{ts.Minutes} minutes ago";
+            }
+
+            if (delta < 90 * MINUTE)
+            {
+                return "an hour ago";
+            }
+
+            if (delta < 24 * HOUR)
+            {
+                return $"{ts.Hours} hours ago";
+            }
+
+            else return $"{ts.Days} days ago";
         }
     }
 
