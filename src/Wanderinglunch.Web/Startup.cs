40ea11a41acebb236a5fc792c6c854bd7256d8e6
Michael Peppler <peppage@gmail.com>
2019-08-31 13:09:02 -0400

    disable hsts

--- src/Wanderinglunch.Web/Startup.cs
+++ src/Wanderinglunch.Web/Startup.cs
@@ -73,7 +73,7 @@ namespace Wanderinglunch.Web
             {
                 app.UseExceptionHandler("/Error");
                 // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-                app.UseHsts();
+                // app.UseHsts();
             }
 
             app.UseRollbarMiddleware();
